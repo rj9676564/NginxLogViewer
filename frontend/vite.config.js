bd7f0f29fb59bd51d6c42812ef82d8b6343f2f64
laibin <laibin@trendit.com>
2026-01-22 11:11:09 +0800

    fix: resolve ESM loading issue in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/resolvers'
 import ComponentsPlugin from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
